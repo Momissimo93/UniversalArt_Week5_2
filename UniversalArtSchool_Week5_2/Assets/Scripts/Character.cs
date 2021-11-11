e4e40d9305f9eaefa17d4e30bde8fafd1a3f499a
Momissimo93 <momo.enrico@hotmail.it>
2021-11-11 18:04:20 +0100

    Hero_Movements
    I am having problems with the Debug.Rays

--- UniversalArtSchool_Week5_2/Assets/Scripts/Character.cs
+++ UniversalArtSchool_Week5_2/Assets/Scripts/Character.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    [SerializeField] protected float speed;
+    [SerializeField] protected float direction;
+    [SerializeField] protected int lifePoint;
+
+    public enum MovementType {PatrollingCheckingGround, HeroMovement};
+    [SerializeField] protected MovementType movementType;
+
+    protected Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,59 @@ public class Character : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Move()
+    {
+        switch (movementType)
+        {
+            case MovementType.PatrollingCheckingGround:
+                PatrollingCheckingGround();
+                break;
+
+            case MovementType.HeroMovement:
+                HeroMovement();
+                break;
+
+            default: break;
+        }
+    }
+    public void PatrollingCheckingGround()
+    {
+
+    }
+    public void HeroMovement()
+    {
+        Rigidbody2D rb;
+
+        if(GetComponent<Rigidbody2D>())
+        {
+            rb = GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(speed * direction, rb.velocity.y);
+        }
+    }
+
+    private protected void StopMoving()
+    {
+        Rigidbody2D rb;
+
+        if (GetComponent<Rigidbody2D>())
+        {
+            Debug.Log("StopMoving");
+            rb = GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(rb.velocity.x * 0, rb.velocity.y);
+
+        }
+    }
+
+    private protected void Jump(int jF)
+    {
+        int jumpForce = jF;
+        Rigidbody2D rb;
+        if (GetComponent<Rigidbody2D>())
+        {
+            rb = GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 }
---
+++ UniversalArtSchool_Week5_2/Assets/Scripts/Hero.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : Character
+{
+    [SerializeField] int jumpForce;
+    private bool facingForward = true;
+    private float speedLocalReference;
+    private bool isFiring;
+
+    int ground = 1 << 6;
+    BoxCollider2D boxCollider2D;
+
+    protected Vector2 leftFoot;
+    protected Vector2 rightFoot;
+
+    private void Awake()
+    {
+        if (GetComponent<BoxCollider2D>())
+        {
+            boxCollider2D = gameObject.GetComponent<BoxCollider2D>();
+
+            leftFoot = new Vector2(boxCollider2D.bounds.min.x, boxCollider2D.bounds.min.y);
+            rightFoot = new Vector2(boxCollider2D.bounds.max.x, boxCollider2D.bounds.min.y);
+            Debug.Log(leftFoot);
+            EmittingRays();
+        }
+    }
+    void Start()
+    {
+        if (GetComponent<Animator>())
+        {
+            animator = gameObject.GetComponent<Animator>();
+            Debug.Log("animator");
+        }
+
+        speedLocalReference = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(leftFoot, Vector2.down * 1f, Color.blue);
+        checkIfButtonDownPressed();
+        EmittingRays();
+    }
+
+    private void FixedUpdate()
+    {
+        checkIfButtonPressed();
+        Debug.DrawRay(leftFoot, Vector2.down * 1f, Color.blue);
+    }
+
+    void checkIfButtonPressed()
+    {
+        if (Input.GetButton("Horizontal") && (!isFiring))
+        {
+            speed = speedLocalReference;
+            SetHeroRotation();
+            direction = Input.GetAxis("Horizontal");
+            animator.SetFloat("speed", speed);
+
+            Move();
+        }
+        else
+        {
+            animator.SetFloat("speed", 0);
+            StopMoving();
+        }
+    }
+
+    void checkIfButtonDownPressed()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire");
+            animator.SetTrigger("fire");
+            isFiring = true;
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            isFiring = false;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            // && ((feet.leftOnGround) || feet.rightOnGround)
+            Debug.Log("Jump");
+            animator.SetTrigger("jump");
+            Jump(jumpForce);
+        }
+    }
+    void SetHeroRotation()
+    {
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            if (facingForward)
+            {
+                transform.Rotate(0f, 180f, 0f);
+                facingForward = false;
+            }
+        }
+        else if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            if (!facingForward)
+            {
+                transform.Rotate(0f, 180f, 0f);
+                facingForward = true;
+            }
+        }
+    }
+    void EmittingRays()
+    {
+        //RaycastHit2D leftFootRays = Physics2D.Raycast(leftFoot, Vector2.down, 3f, ground);
+        Debug.DrawRay(leftFoot, Vector2.down * 1f, Color.blue);
+        Debug.Log("EmittingRays");
+    }
+}
